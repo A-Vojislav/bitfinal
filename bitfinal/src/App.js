cad9e607b4f630d69c59e16b22ca780fa4fb2a6b
A-Vojislav <115400420+A-Vojislav@users.noreply.github.com>
2023-03-25 09:34:05 +0100

    begining

--- bitfinal/src/App.js
+++ bitfinal/src/App.js
@@ -1,26 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import Blabla from './components/Blabla';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          1111
-        </a>
-        <Blabla />
-      </header>
+    <div>
+
     </div>
   );
 }
