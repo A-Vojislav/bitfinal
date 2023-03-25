b1ae6c789676a2e5f8ce5cb39ccd1c67263e354b
DESKTOP-EF2VHJM\ivana <dobrevolje1@gmail.com>
2023-03-25 10:49:54 +0100

    added header and footer

--- bitfinal/src/App.js
+++ bitfinal/src/App.js
@@ -1,9 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/pages/Header";
+import Footer from "./components/pages/Footer";
 
 function App() {
   return (
     <div>
-
+   <Routes>
+    <Route path="/" element={<Header></Header>}></Route>
+    <Route path="/" element={<Footer></Footer>}></Route>
+   </Routes>
     </div>
   );
 }
